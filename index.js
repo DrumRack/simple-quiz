5608651d2658a2ca9b8b80f810d6d49f09d54e4f
Mikhail Kolesnikov <redrumrack@gmail.com>
2025-09-25 05:12:52 +0300

    Complete quiz mechanics implementation

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const startButton = document.getElementById('start')
+const contentSection = document.getElementById('content')
+const startButton = document.getElementById('start-button')
+const textBox = document.createElement('div')
+const answerList = document.createElement('ul')
+let correctAnswers
 
+textBox.classList.add('text')
 startButton.onclick = function () {
-    const question = document.createElement('div')
-    const answerList = document.createElement('ul')
-    const randomIndex = Math.floor(Math.random() * questions.length)
-    
-    question.textContent = questions[randomIndex].question
-    for (let i = 0; i <= 3; i++) getAnswer(randomIndex, i)
-
-    document.getElementById('content').appendChild(question)
-    document.getElementById('content').appendChild(answerList)
-    
+    const copyQuestionList = [...questionList]
+    correctAnswers = 0
+    contentSection.appendChild(textBox)
+    contentSection.appendChild(answerList)
     startButton.remove()
+    getQuestion()
 
-    function getAnswer(questionIndex, answerIndex) {
-        const answer = document.createElement('li')
-        const button = document.createElement('button')
-        button.textContent = questions[questionIndex].answers[answerIndex].text
-        answer.appendChild(button)
-        answerList.appendChild(answer)
+    function getQuestion() {
+        const randomIndex = Math.floor(Math.random() * copyQuestionList.length)
+        textBox.textContent = copyQuestionList[randomIndex].question
+        for (let i = 0; i <= 3; i++) getAnswer(randomIndex, i)
+
+        function getAnswer(questionIndex, answerIndex) {
+            const answer = document.createElement('li')
+            const button = document.createElement('button')
+
+            button.textContent = copyQuestionList[questionIndex].answers[answerIndex].text
+            button.onclick = function () {
+                if (copyQuestionList[questionIndex].answers[answerIndex].isCorrect) {
+                    alert('Вы ответили правильно')
+                    correctAnswers++
+                } else alert('Не верный ответ')
+                answerList.innerHTML = ''
+                copyQuestionList.splice(randomIndex, 1)
+
+                if (copyQuestionList.length !== 0) getQuestion()
+                else {
+                    answerList.remove()
+                    textBox.textContent = `Вы ответили правильно на ${correctAnswers} вопросов`
+                    startButton.textContent = 'Перезапустить'
+                    startButton.style.margin = '50px 0 0 0'
+                    contentSection.appendChild(startButton)
+                }
+            }
+
+            answer.appendChild(button)
+            answerList.appendChild(answer)
+        }
     }
 }
